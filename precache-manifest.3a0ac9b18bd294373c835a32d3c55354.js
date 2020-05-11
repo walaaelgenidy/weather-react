fdc3eaad2cb4172dcba535a7f224330e5332d0e7
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-05-12 01:07:33 +0200

    Updates

---
+++ precache-manifest.3a0ac9b18bd294373c835a32d3c55354.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf50a0ea44d6ac5df929f9e103af8e8f",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "ebcab527f6369173264b",
+    "url": "/weather-react/static/css/main.182fb2d3.chunk.css"
+  },
+  {
+    "revision": "119f8b326d08107942e3",
+    "url": "/weather-react/static/js/2.c831ed82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.c831ed82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebcab527f6369173264b",
+    "url": "/weather-react/static/js/main.860898e6.chunk.js"
+  },
+  {
+    "revision": "0a48121894f93b62268c",
+    "url": "/weather-react/static/js/runtime-main.a23709c0.js"
+  },
+  {
+    "revision": "83dd935016b0eb9b4f88a133e0822b2c",
+    "url": "/weather-react/static/media/daoudi-aissa-Pe1Ol9oLc4o-unsplash.83dd9350.jpg"
+  },
+  {
+    "revision": "f4803e07c4296dc1158a572602fa93cb",
+    "url": "/weather-react/static/media/julien-di-majo-cwL_DmFQTMI-unsplash.f4803e07.jpg"
+  }
+]);
